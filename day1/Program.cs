6a7121430a3bde87cb4517fecfcf4e129750a3e2
Simon Bernsdorff Wallstedt <simon.bernsdorff@outlook.com>
2022-12-03 14:38:08 +0100

    first commit

---
+++ day1/Program.cs
@@ -0,0 +1,19 @@
+﻿List<string> lines = System.IO.File.ReadLines(@"input.txt").ToList();
+
+int counter = 0;
+List<int> nums = new List<int>();
+foreach (string line in lines)
+{
+    if (string.IsNullOrEmpty(line))
+    {
+        nums.Add(counter);
+        counter = 0;
+        continue;
+    }
+    counter += Int32.Parse(line);
+}
+
+nums.Sort();
+nums.Reverse();
+
+Console.WriteLine(nums[0] + nums[1] + nums[2]);
