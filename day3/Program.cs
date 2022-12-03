33809e72bdc6351648ad5784da913acba3315089
Simon Bernsdorff Wallstedt <simon.bernsdorff@outlook.com>
2022-12-03 18:20:35 +0100

    puzzle 2 day 3

--- day3/Program.cs
+++ day3/Program.cs
@@ -1,31 +1,30 @@
-﻿List<string> lines = System.IO.File.ReadLines(@"input.txt").ToList();
+﻿//puzzle 1
+List<string> lines = System.IO.File.ReadLines(@"input.txt").ToList();
 List<string> foundMatches = new List<string>();
 
 string match = "";
 string alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 int score = 0;
 
-foreach (var item in lines)
+int end = 3;
+
+
+for (int i = 0; i < lines.Count; end = end + 3)
 {
-    var first = item.Substring(0, (int)(item.Length / 2));
-    var last = item.Substring((int)(item.Length / 2), (int)(item.Length / 2));
-
-    System.Console.WriteLine(first);
-    System.Console.WriteLine(last);
-
-    foreach (var c in first)
+    foreach (var c in lines[i])
     {
-        if (last.Contains(c))
+        if (lines[i + 1].Contains(c) && lines[i + 2].Contains(c))
         {
             match = c.ToString();
         }
     }
-    System.Console.WriteLine(match);
     foundMatches.Add(match);
-    System.Console.WriteLine("--------next rucksack--------");
-
+    System.Console.WriteLine(match);
+    System.Console.WriteLine("-------end-------");
+    i = i + 3;
 }
-System.Console.WriteLine("end");
+
+
 foreach (var item in foundMatches)
 {
     System.Console.WriteLine(item);
