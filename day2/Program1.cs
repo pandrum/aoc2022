7ca8e2d0b49406f1d095d3d0759cb3cfc7a068e6
Simon Bernsdorff Wallstedt <simon.bernsdorff@outlook.com>
2022-12-03 16:49:45 +0100

    day 3 first puzzla

--- day2/Program1.cs
+++ day2/Program1.cs
@@ -9,80 +9,80 @@ int scissor = 3;
 int draw = 3;
 int win = 6;
 
-// for (int i = 0; i < lines.Count(); i++)
-// {
-//     string opponent = Char.ToString(lines[i][0]);
-//     string you = Char.ToString(lines[i][2]);
+for (int i = 0; i < lines.Count(); i++)
+{
+    string opponent = Char.ToString(lines[i][0]);
+    string you = Char.ToString(lines[i][2]);
 
-//     System.Console.WriteLine(opponent);
-//     System.Console.WriteLine(you);
-
-
-//     /* ROCKS */
-//     //rock against rock
-//     if (opponent.Equals("A") && you.Equals("X"))
-//     {
-//         System.Console.WriteLine("rock against your rock - draw");
-//         yourScore += (draw + rock);
-//     }
-//     //rock against paper
-//     if (opponent.Equals("A") && you.Equals("Y"))
-//     {
-//         System.Console.WriteLine("rock against your paper - win");
-//         yourScore += (win + paper);
-
-//     }
-//     //rock against scissor
-//     if (opponent.Equals("A") && you.Equals("Z"))
-//     {
-//         System.Console.WriteLine("rock against your scissor - lose");
-//         yourScore += (scissor);
-//     }
+    System.Console.WriteLine(opponent);
+    System.Console.WriteLine(you);
 
 
-//     /* PAPER */
-//     //paper against rock
-//     if (opponent.Equals("B") && you.Equals("X"))
-//     {
-//         System.Console.WriteLine("paper against your rock - lose");
-//         yourScore += (rock);
-//     }
-//     //paper against paper
-//     if (opponent.Equals("B") && you.Equals("Y"))
-//     {
-//         System.Console.WriteLine("paper against your paper - draw");
-//         yourScore += (draw + paper);
+    /* ROCKS */
+    //rock against rock
+    if (opponent.Equals("A") && you.Equals("X"))
+    {
+        System.Console.WriteLine("rock against your rock - draw");
+        yourScore += (draw + rock);
+    }
+    //rock against paper
+    if (opponent.Equals("A") && you.Equals("Y"))
+    {
+        System.Console.WriteLine("rock against your paper - win");
+        yourScore += (win + paper);
 
-//     }
-//     //rock against scissor
-//     if (opponent.Equals("B") && you.Equals("Z"))
-//     {
-//         System.Console.WriteLine("paper against your scissor - win");
-//         yourScore += (win + scissor);
-//     }
+    }
+    //rock against scissor
+    if (opponent.Equals("A") && you.Equals("Z"))
+    {
+        System.Console.WriteLine("rock against your scissor - lose");
+        yourScore += (scissor);
+    }
 
-//     /* SCISSOR */
-//     //scissor against rock
-//     if (opponent.Equals("C") && you.Equals("X"))
-//     {
-//         System.Console.WriteLine("scissor against your rock - win");
-//         yourScore += (win + rock);
-//     }
-//     //scissor against paper
-//     if (opponent.Equals("C") && you.Equals("Y"))
-//     {
-//         System.Console.WriteLine("scissor against your paper - lose");
-//         yourScore += (paper);
 
-//     }
-//     //scissor against scissor
-//     if (opponent.Equals("C") && you.Equals("Z"))
-//     {
-//         System.Console.WriteLine("scissor against your scissor - draw");
-//         yourScore += (draw + scissor);
-//     }
-// }
-// System.Console.WriteLine(yourScore);
+    /* PAPER */
+    //paper against rock
+    if (opponent.Equals("B") && you.Equals("X"))
+    {
+        System.Console.WriteLine("paper against your rock - lose");
+        yourScore += (rock);
+    }
+    //paper against paper
+    if (opponent.Equals("B") && you.Equals("Y"))
+    {
+        System.Console.WriteLine("paper against your paper - draw");
+        yourScore += (draw + paper);
+
+    }
+    //rock against scissor
+    if (opponent.Equals("B") && you.Equals("Z"))
+    {
+        System.Console.WriteLine("paper against your scissor - win");
+        yourScore += (win + scissor);
+    }
+
+    /* SCISSOR */
+    //scissor against rock
+    if (opponent.Equals("C") && you.Equals("X"))
+    {
+        System.Console.WriteLine("scissor against your rock - win");
+        yourScore += (win + rock);
+    }
+    //scissor against paper
+    if (opponent.Equals("C") && you.Equals("Y"))
+    {
+        System.Console.WriteLine("scissor against your paper - lose");
+        yourScore += (paper);
+
+    }
+    //scissor against scissor
+    if (opponent.Equals("C") && you.Equals("Z"))
+    {
+        System.Console.WriteLine("scissor against your scissor - draw");
+        yourScore += (draw + scissor);
+    }
+}
+System.Console.WriteLine(yourScore);
 
 
 // puzzle 2
---
+++ day3/Program.cs
@@ -0,0 +1,36 @@
+﻿List<string> lines = System.IO.File.ReadLines(@"input.txt").ToList();
+List<string> foundMatches = new List<string>();
+
+string match = "";
+string alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+int score = 0;
+
+foreach (var item in lines)
+{
+    var first = item.Substring(0, (int)(item.Length / 2));
+    var last = item.Substring((int)(item.Length / 2), (int)(item.Length / 2));
+
+    System.Console.WriteLine(first);
+    System.Console.WriteLine(last);
+
+    foreach (var c in first)
+    {
+        if (last.Contains(c))
+        {
+            match = c.ToString();
+        }
+    }
+    System.Console.WriteLine(match);
+    foundMatches.Add(match);
+    System.Console.WriteLine("--------next rucksack--------");
+
+}
+System.Console.WriteLine("end");
+foreach (var item in foundMatches)
+{
+    System.Console.WriteLine(item);
+    score += (alphabet.IndexOf(item) + 1);
+
+}
+
+System.Console.WriteLine(score);
