2c18565ce80e83364c849d24eec2cfa50b6b768a
Simon Bernsdorff Wallstedt <simon.bernsdorff@outlook.com>
2022-12-11 17:04:59 +0100

    day 6 puzzle 1

---
+++ day6/Program.cs
@@ -0,0 +1,37 @@
+﻿string stream = System.IO.File.ReadAllText(@"input.txt");
+
+int start = 0;
+
+for (int i = 0; i < stream.Length; i++)
+{
+
+    string slice = stream.Substring(start, 4);
+    System.Console.WriteLine($"Slice: {slice}");
+
+    bool unique = true;
+
+    for (int j = 0; j < slice.Length; j++)
+    {
+
+        System.Console.WriteLine(slice[j]);
+        for (int k = j + 1; k < slice.Length; k++)
+        {
+            if (slice[j] == slice[k])
+            {
+                System.Console.WriteLine($"{slice[j]} and {slice[k]} matched. Not unique.");
+                unique = false;
+            }
+        }
+    }
+
+    System.Console.WriteLine(unique);
+
+    if (unique)
+    {
+        System.Console.WriteLine($"Answer: {start + 4}");
+        break;
+    }
+
+    System.Console.WriteLine("==========");
+    start++;
+}
