4d883394cb8b3bdc4a745bbeeeedf1f203b37ebc
Simon Bernsdorff Wallstedt <simon.bernsdorff@outlook.com>
2022-12-30 18:17:02 +0100

    amend

--- day7/Program.cs
+++ day7/Program.cs
@@ -1,50 +1,82 @@
 ﻿List<string> inputs = System.IO.File.ReadLines(@"input.txt").ToList();
-List<string> history = new List<string>();
 
-for (int i = 0; i < inputs.Count; i++)
+List<string> path = new List<string>();
+
+
+string createCurrentDirectory()
 {
-    long currentDirSize = 0;
-    string[] dirs;
-    string dirname;
+    string dir = string.Join("/", path);
+    dir = dir.Replace("//", "/");
+    return dir;
+}
 
-    // if input starts with cd
-    if (inputs[i].StartsWith("$ cd"))
+Dictionary<string, long> folders = new Dictionary<string, long>();
+
+foreach (var input in inputs)
+{
+    if (input.Substring(0, 1).Equals("$"))
     {
-        dirs = inputs[i].Split(" ");
-        dirname = dirs[2];
+        var command = input.Split(" ");
 
-        if (dirname.Equals(".."))
+        if (command[1] == "cd")
         {
-            history.RemoveAt(history.Count - 1);
+            if (command[2] == "..")
+            {
+                path.RemoveAt(path.Count - 1);
+            }
+            else
+            {
+                path.Add(command[2]);
+            }
         }
-        else
+
+        else if (command[1] == "ls")
         {
-            history.Add(dirname);
+            //
         }
     }
 
-    // if input starts with ls
-    if (inputs[i].Equals("$ ls"))
+    else if (input.StartsWith("dir"))
     {
-        for (int j = i + 1; j < inputs.Count; j++)
+        //
+    }
+
+    else
+    {
+        var file = input.Split(" ");
+        if (!folders.ContainsKey(createCurrentDirectory()))
         {
-            if (inputs[j].StartsWith("$"))
-            {
-                break;
-            }
-
-            if (Char.IsDigit(inputs[j][0]))
-            {
-                string[] files = inputs[j].Split(" ");
-                int size = Convert.ToInt32(files[0]);
-                currentDirSize += size;
-            }
-
+            folders[createCurrentDirectory()] = 0;
         }
 
-        System.Console.WriteLine($"Adding {history.Last()} with size {currentDirSize} to list. Level is {history.Count}");
+        string dir = createCurrentDirectory();
+        while (dir.Contains("/"))
+        {
+            System.Console.WriteLine(dir);
+            if (!folders.ContainsKey(dir))
+            {
+                folders[dir] = 0;
+            }
+            folders[dir] += Convert.ToInt32(file[0]);
 
+            if (dir == "/") break;
 
-        System.Console.WriteLine("===================");
+            var dirSplit = dir.Split("/").ToList();
+            dirSplit.RemoveAt(dirSplit.Count - 1);
+            dir = string.Join("/", dirSplit);
+        }
+
     }
 }
+
+
+long total = 0;
+foreach (var item in folders)
+{
+    if (item.Value <= 100000)
+    {
+        total += item.Value;
+    }
+    System.Console.WriteLine($"{item.Key}: {item.Value}");
+}
+System.Console.WriteLine(total);
